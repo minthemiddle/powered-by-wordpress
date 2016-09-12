599552e85e32d426251f80765b80fa8d161c4a75
Martin Betz <martinmichaelbetz@gmail.com>
2016-09-12 18:34:05 +0200

    UPDATE script count entries

--- scripts/count_entries.sh
+++ scripts/count_entries.sh
@@ -6,3 +6,6 @@ awk '/## Commercial/{flag=1;next}/# Contribute/{flag=0}flag' Readme.md
 
 # count entries
 less count.txt | grep ^- | wc -l
+
+# combined, but wget is required to download the file first
+awk '/## Commercial/{flag=1;next}/# Contribute/{flag=0}flag' ~/Readme.md | grep ^- | wc -l
