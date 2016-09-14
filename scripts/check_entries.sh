4f94341f937358eec5110599e57dde44b4ccba0d
Martin Betz <martinmichaelbetz@gmail.com>
2016-09-14 16:17:23 +0200

    UPDATE check entry script
    - Now outputs CSV
    - Now has two arguments: 1=Input, 2=Output
    - Still needs empty last line to process last entry

--- scripts/check_entries.sh
+++ scripts/check_entries.sh
@@ -1,10 +1,10 @@
 # 1=input, 2=output
 
 while read p; do
-	if wad -u "$p" | grep WordPress -c > /dev/null; then
-		printf "$p: YES\n"
+	if wad -u "$p" 2>/dev/null | grep WordPress -c > /dev/null; then
+		printf "$p,YES\n" >> $2
 	else
-		printf "$p: NO\n"
+		printf "$p,NO\n" >> $2
 	fi
 done <$1
 
